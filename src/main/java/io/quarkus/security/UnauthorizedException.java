1a9d1f3e888d680133fb7207e7c8b46ffbf14a22
Sergey Beryozkin <sberyozkin@gmail.com>
2019-11-28 11:01:13 +0000

    Adding the constructors to the exception classes

--- src/main/java/io/quarkus/security/UnauthorizedException.java
+++ src/main/java/io/quarkus/security/UnauthorizedException.java
@@ -4,4 +4,19 @@ package io.quarkus.security;
  * @author Michal Szynkiewicz, michal.l.szynkiewicz@gmail.com
  */
 public class UnauthorizedException extends SecurityException {
+    public UnauthorizedException() {
+
+    }
+
+    public UnauthorizedException(String errorMessage) {
+        this(errorMessage, null);
+    }
+
+    public UnauthorizedException(Throwable cause) {
+        this(null, cause);
+    }
+
+    public UnauthorizedException(String errorMessage, Throwable cause) {
+        super(errorMessage, cause);
+    }
 }
